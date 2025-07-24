03d0b4f5542e94092d686d2f6675a83c0359e014
binod222 <kbimal150@gmail.com>
2025-07-24 12:07:19 +0545

    password validation indlude one uppercase

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,65 +3,99 @@ import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import createToken from "../utils/createToken.js";
 
+/**
+ * Password validation helper
+ */
+function validatePassword(password) {
+  const minLength = 8;
+  const maxLength = 20;
+
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#\$%\^&\*]/.test(password);
+
+  if (password.length < minLength || password.length > maxLength) {
+    return `Password must be between ${minLength} and ${maxLength} characters.`;
+  }
+
+  if (!hasUpperCase) return "Password must include at least one uppercase letter.";
+
+
+  return null; // valid
+}
+
+/**
+ * @desc    Register new user
+ */
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
+    res.status(400);
     throw new Error("Please fill all the fields");
   }
 
-  const userExists = await User.findOne({ email });
-  if (userExists) res.status(400).send("User already exists");
+  const passwordError = validatePassword(password);
+  if (passwordError) {
+    res.status(400);
+    throw new Error(passwordError);
+  }
 
-  // Hash the user password
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   const newUser = new User({ username, email, password: hashedPassword });
 
-  try {
-    await newUser.save();
-    createToken(res, newUser._id);
+  await newUser.save();
+  createToken(res, newUser._id);
 
-    res.status(201).json({
-      _id: newUser._id,
-      username: newUser.username,
-      email: newUser.email,
-      isAdmin: newUser.isAdmin,
-    });
-  } catch (error) {
-    res.status(400);
-    throw new Error("Invalid user data");
-  }
+  res.status(201).json({
+    _id: newUser._id,
+    username: newUser.username,
+    email: newUser.email,
+    isAdmin: newUser.isAdmin,
+  });
 });
 
+/**
+ * @desc    Login user
+ */
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-
-    if (isPasswordValid) {
-      createToken(res, existingUser._id);
-
-      res.status(201).json({
-        _id: existingUser._id,
-        username: existingUser.username,
-        email: existingUser.email,
-        isAdmin: existingUser.isAdmin,
-      });
-    } else {
-      res.status(401).json({ message: "Invalid Password" });
-    }
-  } else {
-    res.status(401).json({ message: "User not found" });
+  if (!existingUser) {
+    res.status(401);
+    throw new Error("User not found");
   }
+
+  const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+
+  if (!isPasswordValid) {
+    res.status(401);
+    throw new Error("Invalid Password");
+  }
+
+  createToken(res, existingUser._id);
+
+  res.status(200).json({
+    _id: existingUser._id,
+    username: existingUser.username,
+    email: existingUser.email,
+    isAdmin: existingUser.isAdmin,
+  });
 });
 
+/**
+ * @desc    Logout user
+ */
 const logoutCurrentUser = asyncHandler(async (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
@@ -71,51 +105,66 @@ const logoutCurrentUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 });
 
+/**
+ * @desc    Get all users
+ */
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
 
+/**
+ * @desc    Get current user profile
+ */
 const getCurrentUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
-  if (user) {
-    res.json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-    });
-  } else {
+  if (!user) {
     res.status(404);
     throw new Error("User not found.");
   }
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  });
 });
 
+/**
+ * @desc    Update current user profile
+ */
 const updateCurrentUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
-  if (user) {
-    user.username = req.body.username || user.username;
-    user.email = req.body.email || user.email;
-
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(req.body.password, salt);
-      user.password = hashedPassword;
-    }
-
-    const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      username: updatedUser.username,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-    });
-  } else {
+  if (!user) {
     res.status(404);
     throw new Error("User not found");
   }
+
+  user.username = req.body.username || user.username;
+  user.email = req.body.email || user.email;
+
+  if (req.body.password) {
+    const passwordError = validatePassword(req.body.password);
+    if (passwordError) {
+      res.status(400);
+      throw new Error(passwordError);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    user.password = hashedPassword;
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({
+    _id: updatedUser._id,
+    username: updatedUser.username,
+    email: updatedUser.email,
+    isAdmin: updatedUser.isAdmin,
+  });
 });
 
 export {
