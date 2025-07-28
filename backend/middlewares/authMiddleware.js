d44daeda4a22b8cbb52ec103029c8bd321cf513f
binod222 <kbimal150@gmail.com>
2025-07-28 10:57:30 +0545

    changes for session management

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,22 +1,56 @@
+// import jwt from "jsonwebtoken";
+// import User from "../models/User.js";
+// import asyncHandler from "./asyncHandler.js";
+
+// // Check if the user is authenticated or not
+// const authenticate = asyncHandler(async (req, res, next) => {
+//   let token;
+
+//   // Read JWT from the 'jwt' cookie
+//   token = req.cookies.jwt;
+
+//   if (token) {
+//     try {
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       req.user = await User.findById(decoded.userId).select("-password");
+//       next();
+//     } catch (error) {
+//       res.status(401);
+//       throw new Error("Not authorized, token failed.");
+//     }
+//   } else {
+//     res.status(401);
+//     throw new Error("Not authorized, no token");
+//   }
+// });
+
+// // Check if the user is admin or not
+// const authorizeAdmin = (req, res, next) => {
+//   if (req.user && req.user.isAdmin) {
+//     next();
+//   } else {
+//     res.status(401).send("Not authorized as an admin");
+//   }
+// };
+
+// export { authenticate, authorizeAdmin };
+
+
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import asyncHandler from "./asyncHandler.js";
 
-// Check if the user is authenticated or not
-const authenticate = asyncHandler(async (req, res, next) => {
-  let token;
-
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+export const authenticate = asyncHandler(async (req, res, next) => {
+  const token = req.cookies.jwt_access;
 
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
       next();
-    } catch (error) {
+    } catch (err) {
       res.status(401);
-      throw new Error("Not authorized, token failed.");
+      throw new Error("Not authorized, invalid token");
     }
   } else {
     res.status(401);
@@ -24,13 +58,11 @@ const authenticate = asyncHandler(async (req, res, next) => {
   }
 });
 
-// Check if the user is admin or not
-const authorizeAdmin = (req, res, next) => {
+export const authorizeAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send("Not authorized as an admin");
+    res.status(401);
+    throw new Error("Not authorized as admin");
   }
 };
-
-export { authenticate, authorizeAdmin };
